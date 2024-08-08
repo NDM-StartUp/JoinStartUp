19267837b5dedc7f4509dd438f8f6f847d78d69e
danilzagor <danilzagor@gmail.com>
2024-08-08 17:03:42 +0200

    first commit

---
+++ src/main/java/ndmstartup/joinstartup/JoinStartUpApplication.java
@@ -0,0 +1,13 @@
+package ndmstartup.joinstartup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinStartUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoinStartUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ndmstartup/joinstartup/JoinStartUpApplicationTests.java
@@ -0,0 +1,13 @@
+package ndmstartup.joinstartup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinStartUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
