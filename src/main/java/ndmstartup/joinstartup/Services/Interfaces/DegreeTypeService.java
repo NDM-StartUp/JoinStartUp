9605bde56d9236b406abb8780c194b174257252c
Mikita Bychkou <nikitoscoin@gmail.com>
2024-09-15 21:27:13 +0200

    Added:DegreeTypeService

---
+++ src/main/java/ndmstartup/joinstartup/Services/Interfaces/DegreeTypeService.java
@@ -0,0 +1,9 @@
+package ndmstartup.joinstartup.Services.Interfaces;
+
+import ndmstartup.joinstartup.DTOs.GetDegreeTypeDTO;
+
+import java.util.List;
+
+public interface DegreeTypeService {
+	List<GetDegreeTypeDTO> getAllDegreeTypes();
+}
