a029420c661f402444139daaa43893ac038195fa
Mikita Bychkou <nikitoscoin@gmail.com>
2024-11-25 01:08:39 +0100

    Created: SupportTicketRepositoryTest.

---
+++ src/test/java/ndmstartup/joinstartup/Repositories/SupportTicketRepositoryTest.java
@@ -0,0 +1,54 @@
+package ndmstartup.joinstartup.Repositories;
+
+import ndmstartup.joinstartup.Models.SupportTicket;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class SupportTicketRepositoryTest {
+	@Autowired
+	private SupportTicketRepository supportTicketRepository;
+
+	@Test
+	public void testFindAllByUserId_ReturnsAllSupportTickets_WhenExist(){
+		List<SupportTicket> tickets = supportTicketRepository.findAllByUserId(1L);
+
+		assertFalse(tickets.isEmpty());
+		assertEquals(1, tickets.size());
+		assertEquals("Issue 1", tickets.get(0).getName());
+		assertEquals("Problem with application", tickets.get(0).getDescription());
+	}
+
+	@Test
+	public void testFindAllByUserId_ReturnsAllSupportTickets_WhenNotExist(){
+		List<SupportTicket> tickets = supportTicketRepository.findAllByUserId(999L);
+
+		assertTrue(tickets.isEmpty());
+	}
+
+	@Test
+	public void testFindAllByStatusName_ReturnsAllSupportTickets_WhenExist() {
+
+		List<SupportTicket> tickets = supportTicketRepository.findAllByStatusName("Closed");
+
+		assertFalse(tickets.isEmpty());
+		assertEquals(1, tickets.size());
+		assertEquals("Issue 2", tickets.get(0).getName());
+		assertEquals("Problem with account", tickets.get(0).getDescription());
+	}
+
+	@Test
+	public void testFindAllByStatusName_ReturnsAllSupportTickets_WhenNotExist() {
+
+		List<SupportTicket> tickets = supportTicketRepository.findAllByStatusName("Abrakadabra");
+
+		assertTrue(tickets.isEmpty());
+	}
+}
